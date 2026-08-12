f8c024bf1cd9fc3b97885f1e5372dc617707b6ca
Leeyunseo <qlwnrdjssl@naver.com>
2026-08-13 01:41:15 +0900

    chore:jwt 수정

--- backend/dari/settings.py
+++ backend/dari/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework.simplejwt",
+    "rest_framework_simplejwt",
     "accounts",
     "home",
     "rehearsal",
