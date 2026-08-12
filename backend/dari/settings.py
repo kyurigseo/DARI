d99615bd2027ff1a346e8ff2aacee676fa8b6388
Leeyunseo <qlwnrdjssl@naver.com>
2026-08-13 01:37:48 +0900

    chore:jwt 세팅 추가

--- backend/dari/settings.py
+++ backend/dari/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +135,4 @@ MAILERS = {
 }
 
 AUTH_USER_MODEL = 'accounts.User'
+SIMPLE_JWT = {"ACCESS_TOKEN_LIFETIME": timedelta(hours=1), "REFRESH_TOKEN_LIFETIME": timedelta(days=14)}
